7b9e5a4a16c2dfe72df6f15f85cfd064d5e90faf
Huan <98620169+bentonaw@users.noreply.github.com>
2024-10-01 02:20:49 +0200

    add some of customer crud operations

---
+++ TableBookingFrontend.NET/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TableBookingFrontend.NET.Models;
+
+namespace TableBookingFrontend.NET.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly HttpClient _client;
+        private string baseUrl = "https://localhost:7043/";
+        public CustomerController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Customers";
+
+            var response = await _client.GetAsync($"{baseUrl}api/Customer");
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var customerList = JsonConvert.DeserializeObject<List<CustomerVM>>(json);
+
+            return View(customerList);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "New Customer";
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerVM customer)
+        {
+            var json = JsonConvert.SerializeObject(customer);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync($"{baseUrl}api/Customer", content);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
