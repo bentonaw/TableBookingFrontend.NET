bc6540ea70a2df765c6a4ebb4d024ef3d5a9a99c
Huan <98620169+bentonaw@users.noreply.github.com>
2024-10-07 00:47:25 +0200

    add cookieoption

--- TableBookingFrontend.NET/Models/CustomerVM.cs
+++ TableBookingFrontend.NET/Models/CustomerVM.cs
@@ -1,11 +1,25 @@
-﻿namespace TableBookingFrontend.NET.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TableBookingFrontend.NET.Models
 {
-    public class CustomerVM
-    {
-        public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-    }
+	public class CustomerVM
+	{
+		public int CustomerId { get; set; }
+		[Required(ErrorMessage = "First name is required")]
+		[StringLength(25, ErrorMessage ="First name cannot exceed 25 characters")]
+		[DisplayName("First Name")]
+		public string FirstName { get; set; }
+		[Required(ErrorMessage = "Last name is required")]
+		[StringLength(50, ErrorMessage = "LastName cannot exceed 50 characters")]
+		[DisplayName("Surname")]
+		public string LastName { get; set; }
+		[EmailAddress(ErrorMessage ="Invalid email format")]
+		public string Email { get; set; }
+		[DisplayName("Phone Number")]
+		[Phone(ErrorMessage ="Invalid phone number")]
+		public string PhoneNumber { get; set; }
+
+		public string FullName => $"{FirstName} {LastName}";
+	}
 }
