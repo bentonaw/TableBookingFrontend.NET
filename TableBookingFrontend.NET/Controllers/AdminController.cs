6abf6faf5752860315e0a857e99f2718347aa177
Huan <98620169+bentonaw@users.noreply.github.com>
2024-10-13 01:31:57 +0200

    add admin menu, WIP

---
+++ TableBookingFrontend.NET/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TableBookingFrontend.NET.Controllers
+{
+	public class AdminController : Controller
+	{
+		private readonly HttpClient _client;
+		private string baseUrl = "https://localhost:7043/";
+        public AdminController(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Admin menu";
+            //TODO
+            // View reservations
+            // View customers
+            // view menu menu
+                // add menu (collection of menuitems)
+                // add menuitems
+                // edit menuitems
+                // quantity especially
+
+            return View();
+        }
+
+    }
+}
